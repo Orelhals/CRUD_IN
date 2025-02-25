f1e54c5a62e93c9d75d4f0d6c01684947a04f5b3
Orelhals <arthurstt22@gmail.com>
2025-02-25 17:46:00 -0300

    Feat: Organizando o código

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
 import fastify from "fastify";
+import { z } from "zod";
+import { appRoutes } from "./http/controllers/routes";
 
 export const app = fastify();
+
+app.get("/home", (request, reply) => {
+  return { message: "Hello world" };
+})
+
+app.register(appRoutes)
---
+++ src/http/controllers/register.ts
@@ -0,0 +1,20 @@
+import { prisma } from "@/lib/prisma";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+    const registerBodySchema = z.object({
+      nome: z.string(),
+      email: z.string().email(),
+      senha: z.string().min(6),
+      foto: z.string().url(),
+    });
+  
+    const {nome, email, senha, foto} = registerBodySchema.parse(request.body);
+  
+    await prisma.usuario.create({
+      data: { nome, email, senha, foto }
+    });
+  
+    return reply.status(201).send({ message: "User created" });
+  }
